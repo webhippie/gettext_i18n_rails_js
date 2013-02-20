966dedb30986ecc184e52065aae3b5911e0620aa
LI Daobing <lidaobing@gmail.com>
2013-02-20 17:37:45 +0800

    don't load parser for java

--- lib/gettext_i18n_rails_js.rb
+++ lib/gettext_i18n_rails_js.rb
@@ -1,6 +1,6 @@
 require 'gettext_i18n_rails'
 require 'gettext_i18n_rails_js/engine'
-require 'gettext_i18n_rails_js/js_and_coffee_parser'
+require 'gettext_i18n_rails_js/js_and_coffee_parser' if RUBY_PLATFORM != 'java'
 
 module GettextI18nRailsJs
 end
